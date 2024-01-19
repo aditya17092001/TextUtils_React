1b10cb47425d7276eefba8496ca0709f64481234
aditya17092001 <adityaudata@gmail.com>
2024-01-20 04:51:47 +0530

    Updates

---
+++ precache-manifest.1f47cb1be888b2e194132cabe49fd942.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e81b19c0622a713de4ee7ae29ffa7009",
+    "url": "/TextUtils_React/index.html"
+  },
+  {
+    "revision": "fcdce7a433718d20d2f5",
+    "url": "/TextUtils_React/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "4ed553b590d31c0651d6",
+    "url": "/TextUtils_React/static/js/2.5453f15b.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/TextUtils_React/static/js/2.5453f15b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3c574b79c2b213597e4",
+    "url": "/TextUtils_React/static/js/3.3dc30ba9.chunk.js"
+  },
+  {
+    "revision": "fcdce7a433718d20d2f5",
+    "url": "/TextUtils_React/static/js/main.413e6bad.chunk.js"
+  },
+  {
+    "revision": "f9917d0afc4fc686f2d0",
+    "url": "/TextUtils_React/static/js/runtime-main.962cb359.js"
+  }
+]);
